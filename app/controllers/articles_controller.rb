8f1c03897fbc2dbcf0458baa3c461281fbf8b91a
Jonathan Norton <Jmichaeln5@gmail.com>
2018-01-03 07:46:45 -0500

    adding Destroy/Delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def index
     @articles = Article.all
   end
@@ -15,7 +14,6 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-
 
   def create
     @article = Article.new(article_params)
@@ -37,9 +35,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-
-end                                             #class end
+end
